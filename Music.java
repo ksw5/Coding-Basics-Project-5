d32af623fc0433459666a9da7f60ae540d412a2f
ksw5 <kieranwindley@gmail.com>
2021-06-26 05:07:50 -0400

    added constructors & test mains to TvShows and Music classes

--- Music.java
+++ Music.java
@@ -1,5 +1,14 @@
 public class Music extends ServiceCommunicator{
-    public Music(String serviceURL) {
-        super(serviceURL);
+    String serviceURL;
+    String musicSearch;
+    public Music(String serviceURL, String musicSearch) {
+        super(serviceURL + musicSearch);
+        this.serviceURL = serviceURL;
+        this.musicSearch = musicSearch;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new ServiceCommunicator("https://itunes.apple.com/search?term=" + "formation" + "\\&limit=1").get());
+        System.out.println(new ServiceCommunicator("https://itunes.apple.com/search?term=" + "bad" + "\\&limit=1").get());
     }
 }
--- TvShows.java
+++ TvShows.java
@@ -1,9 +1,15 @@
 public class TvShows extends ServiceCommunicator{
-    String show;
     String serviceURL;
+    String show;
+
     public TvShows(String serviceURL, String show) {
-        super(serviceURL);
+        super(serviceURL + show);
         this.serviceURL = serviceURL;
         this.show = show;
+    }
+
+    public static void main(String[] argv) {
+        System.out.println(new TvShows("http://api.tvmaze.com/singlesearch/shows?q=", "Family+Matters").get());
+        System.out.println(new TvShows("http://api.tvmaze.com/singlesearch/shows?q=", "blackish").get());
     }
 }
