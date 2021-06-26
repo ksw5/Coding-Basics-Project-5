9f195e3db3b5c9a5e4e37ee303cc8395b5e05f78
ksw5 <kieranwindley@gmail.com>
2021-06-26 04:23:40 -0400

    added constructor and test main method to ZipCode

--- ZipCode.java
+++ ZipCode.java
@@ -1,2 +1,20 @@
-public class ZipCode {
+public class ZipCode extends ServiceCommunicator {
+    public String zipcode;
+    public String serviceURL;
+
+    public ZipCode(String serviceURL, String zipcode) {
+        super(serviceURL + zipcode);
+        this.zipcode = zipcode;
+        this.serviceURL = serviceURL;
+    }
+
+    /*
+    Main method to test this class
+     */
+    public static void main(String[] argv) {
+        System.out.println(new ZipCode( URLstring + "10011"));
+        System.out.println(new ServiceCommunicator("https://itunes.apple.com/search?term=" + "bbking" + "\\&limit=1").get());
+        System.err.println("==================================");
+        System.out.println(new ServiceCommunicator("http://www.google.com").get());
+    }
 }
